fd1f41f248358a676ac4dedb471646b1f1d9191f
Arvind Kharwal <karvind629@gmail.com>
2026-04-08 15:55:53 +0530

    8 April 2026

--- views/profile.py
+++ views/profile.py
@@ -2,7 +2,7 @@ import streamlit as st;
 import pandas as pd
 st.title('Profile')
 col1,col2,col3 = st.columns(3)
-col1.image('assets\moon.png',width=100)
+col1.image('assets/moon.png',width=100)
 col3.subheader("My Name: ")
 
 st.divider()
