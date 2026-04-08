5a4355c50da42f0a6c26ed97da12c5eb38b98738
Arvind Kharwal <karvind629@gmail.com>
2026-04-08 15:59:01 +0530

    8 April 2026

--- views/sample.py
+++ views/sample.py
@@ -1,2 +1,7 @@
 import streamlit as st;
+import pandas as pd
+import streamlit as st
+from numpy.random import default_rng as rng
 st.title('Home')
+df = pd.DataFrame(rng(0).standard_normal((20, 3)), columns=["a", "b", "c"])
+st.line_chart(df)
