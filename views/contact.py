f40a62b1e420815e55d5d6b0d327fa7ca11bbc2d
Arvind Kharwal <karvind629@gmail.com>
2026-04-08 15:59:58 +0530

    8 April 2026

--- views/contact.py
+++ views/contact.py
@@ -1,2 +1,9 @@
 import streamlit as st;
+
+import streamlit as st
 st.title('Contact')
+enable = st.checkbox("Enable camera")
+picture = st.camera_input("Take a picture", disabled=not enable)
+
+if picture:
+    st.image(picture)
